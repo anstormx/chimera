6a56b9a5c54b07d5d7cfc90e4be99a8d69e1a6c0
Aniket <aniketkini1@gmail.com>
2024-07-04 22:55:13 +0530

    updated packages

--- config-overrides.js
+++ config-overrides.js
@@ -1,28 +1,25 @@
-const webpack = require('webpack'); 
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+import webpack from 'webpack';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        os: require.resolve('os-browserify'),
+      };
 
-module.exports = function override(config) { 
-  const fallback = config.resolve.fallback || {}; 
-  Object.assign(fallback, { 
-    "crypto": require.resolve("crypto-browserify"), 
-    "stream": require.resolve("stream-browserify"), 
-    "assert": require.resolve("assert"), 
-    "http": require.resolve("stream-http"), 
-    "https": require.resolve("https-browserify"), 
-    "os": require.resolve("os-browserify"), 
-    "url": require.resolve("url"),
-    "path": require.resolve("path-browserify"),
-    "vm": require.resolve("vm-browserify"),
-    "process": require.resolve("process/browser"),
-  }) 
-  config.resolve.fallback = fallback; 
-  config.plugins = (config.plugins || []).concat([ 
-    new webpack.ProvidePlugin({ 
-      process: 'process/browser', 
-      Buffer: ['buffer', 'Buffer'] 
-    }),
-    new NodePolyfillPlugin() 
-  ]) 
-  return config; 
-}
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+        new NodePolyfillPlugin()
+      );
+    }
+
+    return config;
+  },
+};
