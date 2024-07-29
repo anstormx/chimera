eea03429060d51e9d29be18851fdda330c5da0d7
Aniket <aniketkini1@gmail.com>
2024-07-30 00:44:24 +0530

    ipfs gateway added

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,8 @@
 export const GetIpfsUrlFromPinata = (pinataUrl) => {
-    var IPFSUrl = pinataUrl.split("/");
-    const lastIndex = IPFSUrl.length;
-    IPFSUrl = "https://ipfs.io/ipfs/"+IPFSUrl[lastIndex-1];
-    return IPFSUrl;
+  var IPFSUrl = pinataUrl.split("/");
+  const lastIndex = IPFSUrl.length;
+  IPFSUrl =
+    "https://pink-naval-cardinal-905.mypinata.cloud/ipfs/" +
+    IPFSUrl[lastIndex - 1];
+  return IPFSUrl;
 };
